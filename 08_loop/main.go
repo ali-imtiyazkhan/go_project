74a22adc16190e7a1f38419b02364e72de77d3d6
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-20 13:17:41 +0530

    feat:adding another loop for in range

--- 08_loop/main.go
+++ 08_loop/main.go
@@ -9,9 +9,20 @@ func main() {
 
 	sum := 0
 
+	// Simple for loop
 	for i := 0; i < len(views); i++ {
 		sum += views[i]
 	}
+
+	total := 0
+
+	// for Range Loop
+	for i, v := range views {
+		fmt.Println("day", i, "views", v)
+		total = total + v
+	}
+
+	fmt.Println(total)
 
 	fmt.Println("Sum:", sum)
 }
