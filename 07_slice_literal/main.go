dad30db3fc11727bd8a6ea09badcc17e74684f4f
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-20 12:58:18 +0530

    adding slice and the cap funvtion

--- 07_slice_literal/main.go
+++ 07_slice_literal/main.go
@@ -7,8 +7,16 @@ func main() {
 	// dynmaic collector can grow
 	// []type{...}
 
+	// make([]T,len,cap)
+
 	result := []int{1, 2, 3, 4, 5}
 	result = append(result, 12)
 
+	score := make([]int, 0, 5)
+	score = append(score, 0, 2)
+
+	fmt.Println(score)
+
 	fmt.Println(result, result[0], len(result))
+
 }
