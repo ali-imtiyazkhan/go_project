cf4f151349c90384ad0e22b4bb98b9ee2306feba
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-20 22:23:08 +0530

    passing so many argument at a time ...

---
+++ 13_variadic_function/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func sumAll(num ...int) int {
+
+	total := 0
+
+	for _, val := range num {
+		total += val
+	}
+
+	return total
+}
+
+func main() {
+	sum := sumAll(1, 2, 3, 4, 5, 6)
+	fmt.Println(sum)
+}
