2e8254e926d1eaa1e77e3db36679473f04bcd9cf
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-21 22:43:29 +0530

    03_json_encoder: add /decode route for JSON decoding

--- HTTP_Mod_files/03_json_encoder/main.go
+++ HTTP_Mod_files/03_json_encoder/main.go
@@ -1,3 +1,54 @@
 package main
 
-func main()
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+
+	res := map[string]any{
+		"ok":       true,
+		"message":  "JSon encode successful",
+		"dateTime": time.Now().UTC(),
+	}
+
+	_ = json.NewEncoder(w).Encode(res)
+}
+
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+
+	var data map[string]any
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(map[string]any{
+			"ok":    false,
+			"error": err.Error(),
+		})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(map[string]any{
+		"ok":      true,
+		"message": "JSON decode successful",
+		"data":    data,
+	})
+}
+
+func main() {
+
+	http.HandleFunc("/ok", rootHandler)
+	http.HandleFunc("/decode", decodeHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	fmt.Println(err)
+}
